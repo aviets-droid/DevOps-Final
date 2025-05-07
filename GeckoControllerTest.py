4f7270a914bac49a1917945e4435231e8be7f565
aviets-droid <aviets@unr.edu>
2025-05-06 18:03:07 -0700

    removed big comment

--- GeckoControllerTest.py
+++ GeckoControllerTest.py
@@ -60,18 +60,6 @@ class TestGeckoController(unittest.TestCase):
         )
         
         self.controller.addGecko(self.mock_gecko)
-        
-        # cur.execute(
-        #     """INSERT INTO usercollection (name, sex, age, morphs, healthInfo) 
-        #     VALUES (%s, %s, %s, %s, %s);""",
-        #     (
-        #         gecko.getName(gecko),
-        #         gecko.getSex(gecko),
-        #         gecko.getAge(gecko),
-        #         gecko.getMorphNames(gecko),
-        #         gecko.getHealthInfo(gecko),
-        #     ),
-        # )
 
         mock_cursor.execute.assert_called_with(
                 "INSERT INTO usercollection ("
